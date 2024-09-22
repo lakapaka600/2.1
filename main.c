03f20be14fa293658e4a395c099f5dd3c594ff2b
Jasss <153324785+lakapaka600@users.noreply.github.com>
2024-09-22 16:28:28 +0200

    c 2.1

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    float num1, num2;
+    printf("put the first number \n");
+     scanf("%f", &num1);
+    printf("put the second number \n");
+     scanf("%f",&num2);
+    float c = (num1+ num2)/2;
+    printf("la media : %f\n","c");
+    return 0;
+}
+
